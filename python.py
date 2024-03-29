5c81f5b68504b58dcf15c2f72d1b565bb6550a01
Kariuki Karanja <kariukimarkbrian@gmail.com>
2024-03-29 13:02:16 +0300

    renamed python file

---
+++ python.py
@@ -0,0 +1,17 @@
+def fizzBuzz(n):
+    result = []
+
+    for i in range(1, n+1):
+        add = ''
+
+        if i % 3 == 0:
+            add += "Fizz"
+
+        if i % 5 == 0:
+            add += 'Buzz'
+
+        if add == '':
+            result.append(i)
+        else:
+            result.append(add)
+    return result
