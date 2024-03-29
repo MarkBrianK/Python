973200b89056fb984c7f94b8db8c9e54af744940
Kariuki Karanja <kariukimarkbrian@gmail.com>
2024-03-29 12:50:57 +0300

    Number divisible by 3 or 5 js

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function fizzBuzz(n){
+    let result = [];
+    for(let i = 1; i<=n; i++){
+        let add ='';
+        
+        if(i % 3 === 0){
+            add +='Fizz';
+
+        }
+        if ( i % 5 === 0){
+            add += 'Buzz';
+        }
+        if (add === ''){
+            result.push(i);
+        }
+        else{
+            result.push(add);
+        }
+    }
+    return result;
+}
+
